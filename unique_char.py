e4c3b1aad1cce4921671895960fc71a42010e208
qzshen <quinn.z.shen@gmail.com>
2013-12-31 02:09:33 -0500

    Unique Char Problem.

---
+++ unique_char.py
@@ -0,0 +1,22 @@
+# Implement an algorithm to determine if a string has all unique characters.
+
+def unique(string):
+    """Return True if string is completely unique. Return False otherwise.
+
+    >>> unique('abc')
+    True
+    >>> unique('abca')
+    False
+    """
+    seen_chars = set()
+    for char in string:
+        if char in seen_chars:
+            return False
+        seen_chars.add(char)
+    return True
+
+# What if you can't use additional data structures?
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
