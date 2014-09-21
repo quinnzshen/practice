f35a74857c4adebba0025078998fe878034abd29
qzshen <quinn.z.shen@gmail.com>
2014-09-21 10:22:49 -0700

    Cleaned up code, added initial docstring, created infection_counter and tests.

--- Khan Academy/infection.py
+++ Khan Academy/infection.py
@@ -1,6 +1,34 @@
 """
 Quinn Z Shen
 Khan Academy Language Agnostic Project
+
+Notes
+=====
+I changed the parameters of limited_infection so that it doesn't
+take in an initial user to infect. Instead, the method purely focuses
+on the threshold or limit of infections we are aiming for. I decided
+to use the limit as an upper-bound, since when ramping-up features I
+figured that it is best to play it safe (i.e. less chance of more users
+discovering a potential bug). The specs said to only infect the
+students of the coach, but I felt that it made more sense to infect
+the entire connected component. Otherwise we risk confusion and
+inconsistancy for users who are both students and teachers (e.g.
+a teacher is taking a class on how to teach a certain class effectively
+using Khan Academy. We want to make sure their experiences are identical).
+
+When implementing limited_infection initially, I didn't understand
+why users would already be infected. It seemed logical to me that
+if Khan Academy was pushing a new version of code to teachers and
+students that they would have complete control and not randomly
+have unexpected 'infections' (i.e. we should be able to control the
+infection so that a connected component is either completely infected
+or not infected at all). It was only until after implementation
+that I realized that a very common case of 'existing infections' can
+occur when ramping up a certain feature. Random 'infections' can also
+appear if adding edges is not carefully implemented. For example,
+if we 'infect' a certain teacher's group of students and then the
+teacher gains a new student, we need to be very careful and ensure
+that when adding a connection, the 'infection' is still passed along.
 """
 
 import operator, unittest
@@ -68,7 +96,21 @@ class KhanNetwork:
         the number of users that were infected.
         """
 
-        return KhanNetwork.infect_cc(self, user, explored=set())
+        return self.infect_cc(user, explored=set())
+
+    def current_infections(self):
+        """Return the number of currently infected users."""
+        root_teachers = self.root_teachers()
+        infected_count = 0
+
+        for teacher in root_teachers:
+            infected_count += self.explore_infection(teacher)
+        return infected_count
+
+    def root_teachers(self):
+        """Return the list of root teachers. Teachers are root teachers
+        if they don't have any teachers (i.e. they are root nodes)."""
+        return [user for user in self.existing_users if not user.teachers]
 
     def infect_cc(self, user, explored):
         """ Helper function that infects the entire connected component.
@@ -80,11 +122,11 @@ class KhanNetwork:
 
         for student in user.students:
             if student not in explored:
-                infected_count += KhanNetwork.infect_cc(self, student, explored)
+                infected_count += self.infect_cc(student, explored)
 
         for teacher in user.teachers:
             if teacher not in explored:
-                infected_count += KhanNetwork.infect_cc(self, teacher, explored)
+                infected_count += self.infect_cc(teacher, explored)
 
         return infected_count
 
@@ -94,13 +136,12 @@ class KhanNetwork:
         close to the limit amount of users. Return number of users infected.
         """
 
-        # Teachers are a root_teacher if they don't have any teachers (i.e. parent nodes)
-        root_teachers = [user for user in self.existing_users if not user.teachers]
+        root_teachers = self.root_teachers()
         options = []
         infection_counter = 0
 
         for teacher in root_teachers:
-            student_count = KhanNetwork.explore(self, teacher, explored=set())
+            student_count = self.explore(teacher)
             options.append((teacher, student_count))
 
         # Sort options by their student_count (i.e. amount of children) in decending order
@@ -108,7 +149,7 @@ class KhanNetwork:
 
         for teacher, count in options:
             if (infection_counter + count) <= limit:
-                KhanNetwork.total_infection(self, teacher)
+                self.total_infection(teacher)
                 infection_counter += count
 
         return infection_counter
@@ -116,54 +157,76 @@ class KhanNetwork:
     def limited_infection_exact(self, limit):
         """
         Attempt to spread the infection (i.e. the current version) to the
-        exact amount of users (specified by the limit). Do nothing if not 
+        exact amount of users (specified by the limit). Do nothing if not
         possible. Return number of users infected.
         """
 
-        # Teachers are a root_teacher if they don't have any teachers (i.e. parent nodes)
-        root_teachers = [user for user in self.existing_users if not user.teachers]
+        root_teachers = self.root_teachers()
         options = []
         infection_counter = 0
 
         for teacher in root_teachers:
-            student_count = KhanNetwork.explore(self, teacher, explored=set())
+            student_count = self.explore(teacher, explored=set())
             options.append((teacher, student_count))
 
         # Discover which users to infect for an exact matching.
-        solution_option = KhanNetwork.find_combination(self, options, limit, partial=[])
+        solution_option = self.find_combination(options, limit, partial=[])
 
         for user in solution_option:
-            infection_counter += KhanNetwork.total_infection(self, user)
+            infection_counter += self.total_infection(user)
 
         return infection_counter
 
-    def explore(self, user, explored):
+    def explore(self, user, explored=None):
         """
         Helper function that performs DFS on the students of the user.
         Returns the size (total children under the given user).
         """
+        if explored is None:
+            explored = set()
 
         explored.add(user)
         explored_count = 1
 
         for student in user.students:
             if student not in explored:
-                explored_count += KhanNetwork.explore(self, student, explored)
+                explored_count += self.explore(student, explored)
 
         return explored_count
 
-    def find_combination(self, options, target, partial=None):
-        """ Find a solution_option containing (teacher, student_count) tuples
-            that will reach our target limit exactly. If not possible return None.
-            Note: If there is more than one solution, it will return the first 
-            solution that the algorithm comes across.
+    def explore_infection(self, user, explored=None):
+        """
+        Helper function that performs DFS on the students of the user.
+        Returns the amount of encountered infections.
+        """
+        if explored is None:
+            explored = set()
 
-            >>> network = KhanNetwork()
-            >>> options = [("A", 2), ("B", 3), ("C", 2), ("D", 5), ("E", 9)]
-            >>> network.find_combination(options, 15, partial=[])
-            []
-            >>> network.find_combination(options, 13, partial=[])
-            ['A', 'C', 'E']
+        explored.add(user)
+        infection_count = 0
+
+        if user.version == KhanNetwork.CURRENT_VERSION:
+            infection_count += 1
+
+        for student in user.students:
+            if student not in explored:
+                infection_count += self.explore_infection(student, explored)
+
+        return infection_count
+
+    def find_combination(self, options, target, partial=None):
+        """
+        Find a solution_option containing (teacher, student_count) tuples
+        that will reach our target limit exactly. If not possible return None.
+        Note: If there is more than one solution, it will return the first
+        solution that the algorithm comes across.
+
+        >>> network = KhanNetwork()
+        >>> options = [("A", 2), ("B", 3), ("C", 2), ("D", 5), ("E", 9)]
+        >>> network.find_combination(options, 15, partial=[])
+        []
+        >>> network.find_combination(options, 13, partial=[])
+        ['A', 'C', 'E']
         """
 
         if partial is None:
@@ -178,7 +241,7 @@ class KhanNetwork:
             for index in xrange(len(options)):
                 option = options[index]
                 remaining_options = options[index + 1:]
-                answer = KhanNetwork.find_combination(self, \
+                answer = self.find_combination( \
                             remaining_options, target, partial + [option])
                 if answer:
                     return answer
@@ -195,6 +258,10 @@ class TestInfection(unittest.TestCase):
         self.user4 = self.network.create_user("1.1", [self.user1, self.user3])
         self.user5 = self.network.create_user("1.1")
         self.user6 = self.network.create_user("1.1", [self.user5])
+        self.user7 = self.network.create_user("1.2")
+        self.user8 = self.network.create_user("1.1", [self.user7])
+        self.user9 = self.network.create_user("1.2")
+        self.user10 = self.network.create_user("1.0", [self.user8, self.user9])
         self.infected = None
         print "--- network at set-up ---"
         self.network.print_userbase()
@@ -265,6 +332,11 @@ class TestInfection(unittest.TestCase):
         """
         self.infected = self.network.limited_infection_exact(5)
         self.assertEqual(self.infected, 0)
+
+    def test_infection_count(self):
+        """Test current_infections() & explore_infection()"""
+        self.infected = self.network.current_infections()
+        self.assertEqual(self.infected, 2)
 
     def tearDown(self):
         print "--- network at tear-down ---"
