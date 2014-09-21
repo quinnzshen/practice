b7e1b739e3c38628627282b6ef68748215264927
qzshen <quinn.z.shen@gmail.com>
2014-09-21 09:37:15 -0700

    Cleaned up code using pylint.

--- Khan Academy/infection.py
+++ Khan Academy/infection.py
@@ -41,22 +41,30 @@ class KhanNetwork:
     def __init__(self):
         self.existing_users = set()
 
-    def create_user(self, version = None, students = [], teachers = []):
+    def create_user(self, version=None, students=None, teachers=None):
+        """Create a Khan User and add them to the database."""
+        if students is None:
+            students = []
+        if teachers is None:
+            teachers = []
         user = User(version, students, teachers)
         self.existing_users.add(user)
         return user
 
     def print_userbase(self):
+        """Print the userbase of the current network."""
         for user in self.existing_users:
             print user
         return True
 
     def total_infection(self, user):
-        """ Spread the infection (i.e. the current version), beginning 
-            at the given user, to the entire connected component. Return 
-            the number of users that were infected. """
+        """
+        Spread the infection (i.e. the current version), beginning
+        at the given user, to the entire connected component. Return
+        the number of users that were infected.
+        """
 
-        return KhanNetwork.infect_cc(self, user, explored = set())
+        return KhanNetwork.infect_cc(self, user, explored=set())
 
     def infect_cc(self, user, explored):
         """ Helper function that infects the entire connected component.
@@ -77,8 +85,10 @@ class KhanNetwork:
         return infected_count
 
     def limited_infection(self, limit):
-        """ Spread the infection (i.e. the current version), and try to infect 
-            close to the limit amount of users. Return number of users infected. """
+        """
+        Spread the infection (i.e. the current version), and try to infect
+        close to the limit amount of users. Return number of users infected.
+        """
 
         # Teachers are a root_teacher if they don't have any teachers (i.e. parent nodes)
         root_teachers = [user for user in self.existing_users if not user.teachers]
@@ -86,13 +96,13 @@ class KhanNetwork:
         infection_counter = 0
 
         for teacher in root_teachers:
-            student_count = KhanNetwork.explore(self, teacher, explored = set())
+            student_count = KhanNetwork.explore(self, teacher, explored=set())
             options.append((teacher, student_count))
 
         # Sort options by their student_count (i.e. amount of children) in decending order
         options.sort(key=operator.itemgetter(1), reverse=True)
 
-        for teacher,count in options:
+        for teacher, count in options:
             if (infection_counter + count) <= limit:
                 KhanNetwork.total_infection(self, teacher)
                 infection_counter += count
@@ -100,9 +110,11 @@ class KhanNetwork:
         return infection_counter
 
     def limited_infection_exact(self, limit):
-        """ Attempt to spread the infection (i.e. the current version) to the 
-            exact amount of users (specified by the limit). Do nothing if not possible.
-            Return number of users infected. """
+        """
+        Attempt to spread the infection (i.e. the current version) to the
+        exact amount of users (specified by the limit). Do nothing if not 
+        possible. Return number of users infected.
+        """
 
         # Teachers are a root_teacher if they don't have any teachers (i.e. parent nodes)
         root_teachers = [user for user in self.existing_users if not user.teachers]
@@ -110,7 +122,7 @@ class KhanNetwork:
         infection_counter = 0
 
         for teacher in root_teachers:
-            student_count = KhanNetwork.explore(self, teacher, explored = set())
+            student_count = KhanNetwork.explore(self, teacher, explored=set())
             options.append((teacher, student_count))
 
         # Discover which users to infect for an exact matching.
@@ -122,8 +134,10 @@ class KhanNetwork:
         return infection_counter
 
     def explore(self, user, explored):
-        """ Helper function that performs DFS on the students of the user. 
-            Returns the size (total children under the given user). """
+        """
+        Helper function that performs DFS on the students of the user.
+        Returns the size (total children under the given user).
+        """
 
         explored.add(user)
         explored_count = 1
@@ -134,7 +148,7 @@ class KhanNetwork:
 
         return explored_count
 
-    def find_combination(self, options, target, partial=[]):
+    def find_combination(self, options, target, partial=None):
         """ Find a solution_option containing (teacher, student_count) tuples
             that will reach our target limit exactly. If not possible return None.
             Note: If there is more than one solution, it will return the first 
@@ -148,16 +162,20 @@ class KhanNetwork:
             ['A', 'C', 'E']
         """
 
-        current_count = sum([count for teacher,count in partial])
+        if partial is None:
+            partial = []
+
+        current_count = sum([count for teacher, count in partial])
         if current_count == target:
-            return [teacher for teacher,count in partial]
+            return [teacher for teacher, count in partial]
         elif current_count > target:
             return []
         else:
             for index in xrange(len(options)):
                 option = options[index]
                 remaining_options = options[index + 1:]
-                answer = KhanNetwork.find_combination(self, remaining_options, target, partial + [option])
+                answer = KhanNetwork.find_combination(self, \
+                            remaining_options, target, partial + [option])
                 if answer:
                     return answer
             return []
@@ -167,61 +185,82 @@ class TestInfection(unittest.TestCase):
 
     def setUp(self):
         self.network = KhanNetwork()
-        self.a = self.network.create_user()
-        self.b = self.network.create_user("1.0")
-        self.c = self.network.create_user("1.0", [self.b], [])
-        self.d = self.network.create_user("1.1", [self.a,self.c])
-        self.e = self.network.create_user("1.1")
-        self.f = self.network.create_user("1.1", [self.e])
+        self.user1 = self.network.create_user()
+        self.user2 = self.network.create_user("1.0")
+        self.user3 = self.network.create_user("1.0", [self.user2], [])
+        self.user4 = self.network.create_user("1.1", [self.user1, self.user3])
+        self.user5 = self.network.create_user("1.1")
+        self.user6 = self.network.create_user("1.1", [self.user5])
+        self.infected = None
         print "--- network at set-up ---"
         self.network.print_userbase()
 
-    def test_test(self):
-        self.assertEqual(1, 1)
-
     def test_total_infection_4(self):
-        self.infected = self.network.total_infection(self.a)
+        """Test total_infection() for cc of 4."""
+        self.infected = self.network.total_infection(self.user1)
         self.assertEqual(self.infected, 4)
 
     def test_total_infection_2(self):
-        self.infected = self.network.total_infection(self.e)
+        """Test total_infection() for cc of 2."""
+        self.infected = self.network.total_infection(self.user5)
         self.assertEqual(self.infected, 2)
 
     def test_limited_infection_1(self):
+        """Test limited_infection(1); should do nothing."""
         self.infected = self.network.limited_infection(1)
         self.assertEqual(self.infected, 0)
 
     def test_limited_infection_2(self):
+        """Test limited_infection(2); should find cc of 2."""
         self.infected = self.network.limited_infection(2)
         self.assertEqual(self.infected, 2)
 
     def test_limited_infection_3(self):
+        """
+        Test limited_infection(3); should find cc of 2
+        and then stop because the other cc is size 4.
+        """
         self.infected = self.network.limited_infection(3)
         self.assertEqual(self.infected, 2)
 
     def test_limited_infection_4(self):
+        """Test limited_infection(4); should find cc of 4."""
         self.infected = self.network.limited_infection(4)
         self.assertEqual(self.infected, 4)
 
     def test_limited_infection_5(self):
+        """
+        Test limited_infection(5); should find cc of 4
+        and then stop because the other cc is size 2.
+        """
         self.infected = self.network.limited_infection(5)
         self.assertEqual(self.infected, 4)
 
     def test_limited_infection_6(self):
+        """
+        Test limited_infection(6); should find cc of
+        both size 4 and size 2 and infect both.
+        """
         self.infected = self.network.limited_infection(6)
         self.assertEqual(self.infected, 6)
 
     def test_exact_infection_2(self):
+        """Test exact_infection for 2."""
         self.infected = self.network.limited_infection_exact(2)
         self.assertEqual(self.infected, 2)
 
     def test_exact_infection_6(self):
+        """Test exact infection for 6."""
         self.infected = self.network.limited_infection_exact(6)
         self.assertEqual(self.infected, 6)
 
     def test_exact_infection_5(self):
+        """
+        Test exact infection for 5. Since this is not
+        possible, no one should become newly infected.
+        """
         self.infected = self.network.limited_infection_exact(5)
-        self.assertEqual(self.infected, 0)     
+        self.assertEqual(self.infected, 0)
 
     def tearDown(self):
         print "--- network at tear-down ---"
